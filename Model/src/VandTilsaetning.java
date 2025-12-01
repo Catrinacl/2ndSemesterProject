0b90f5f67952b31aa2d33d6cbba2145e614f6b45
Timka1001 <eaa23tsd@students.eaaa.dk>
2025-12-01 10:38:51 +0100

    ret VandTilsaetning

--- Model/src/VandTilsaetning.java
+++ Model/src/VandTilsaetning.java
@@ -2,26 +2,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class VandTilsaetning {
+    private String vandTilsaetning;
+    private double vandMaengde;
+    private String vandKilde;
+    private WhiskyProdukt whiskyProdukt;
 
-    private String produktNr;
-    private String navn;
-    private String beskrivelse;
-    private double slutAlkoholProcent;
-    private boolean erSingleCask;
-    private int antalFlasker;
-    private VandTilsaetning vandTilsaetning;
-    private List<Aftapning> aftapninger;
-
-    public WhiskyProdukt(String produktNr, String navn, String beskrivelse, double slutAlkoholProcent,
-                         boolean erSingleCask, int antalFlasker, VandTilsaetning vandTilsaetning) {
-        this.produktNr = produktNr;
-        this.navn = navn;
-        this.beskrivelse = beskrivelse;
-        this.slutAlkoholProcent = slutAlkoholProcent;
-        this.erSingleCask = erSingleCask;
-        this.antalFlasker = antalFlasker;
+    public VandTilsaetning(String vandTilsaetning, double vandMaengde,
+                           String vandKilde, WhiskyProdukt whiskyProdukt) {
         this.vandTilsaetning = vandTilsaetning;
-        this.aftapninger = new ArrayList<>();
+        this.vandMaengde = vandMaengde;
+        this.vandKilde = vandKilde;
+        this.whiskyProdukt = whiskyProdukt;
     }
-
 }
