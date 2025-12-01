728990142394728b19e6082ef0bf8bef76d026cc
Gyldigt <Andershoffmann@gmail.com>
2025-12-01 23:25:32 +0100

    Commit - Anders Storage rettet lidt 1.sem Controller

--- Projekt/src/Controller/Controller.java
+++ Projekt/src/Controller/Controller.java
@@ -1,4 +1,177 @@
 package Controller;
 
+import Model.*;
+import Storage.Storage;
+
+import java.util.ArrayList;
+import java.time.LocalDate;
+
 public class Controller {
+
+    private Storage storage;
+
+    public Controller() {
+        storage = new Storage();
+    }
+
+
+    public Lager createLager(String lagerId, String lagerType, String adresse, ArrayList<Reol> reoler) {
+        Lager lager = new Lager(lagerId, lagerType, adresse, reoler);
+        storage.addLager(lager);
+        return lager;
+    }
+
+    public ArrayList<Lager> getLagre() {
+        return storage.getLagre();
+    }
+
+    public Reol createReol(String reolId, String reolType, ArrayList<Hylde> hylder, Lager lager) {
+        Reol reol = new Reol(reolId, reolType, hylder, lager);
+        storage.addReol(reol);
+        return reol;
+    }
+
+    public ArrayList<Reol> getReoler() {
+        return storage.getReoler();
+    }
+
+    public Hylde createHylde(String hyldeId, int kapacitet, String placering, String hyldeType,
+                             ArrayList<Fad> fade, Reol reol) {
+        Hylde hylde = new Hylde(hyldeId, kapacitet, placering, hyldeType, fade, reol);
+        storage.addHylde(hylde);
+        return hylde;
+    }
+
+    public ArrayList<Hylde> getHylder() {
+        return storage.getHylder();
+    }
+
+    public Fad createFad(String fadId, double stoerrelseL, String traeType, String tidligereIndhold,
+                         String status, ArrayList<Paafyldning> paafyldninger, Hylde hylde) {
+        Fad fad = new Fad(fadId, stoerrelseL, traeType, tidligereIndhold, status, paafyldninger, hylde);
+        storage.addFad(fad);
+        return fad;
+    }
+
+    public ArrayList<Fad> getFade() {
+        return storage.getFade();
+    }
+
+
+    public Destillering createDestillering(String distilleringsID,
+                                           LocalDate startDato,
+                                           LocalDate slutDato,
+                                           String maltBatch,
+                                           String kornsort,
+                                           String rygemateriale,
+                                           String kommentar,
+                                           ArrayList<MaengdeDestilleret> maengder) {
+        Destillering d = new Destillering(distilleringsID, startDato, slutDato,
+                maltBatch, kornsort, rygemateriale, kommentar, maengder);
+        storage.addDestillering(d);
+        return d;
+    }
+
+    public ArrayList<Destillering> getDestilleringer() {
+        return storage.getDestilleringer();
+    }
+
+    public Destillat createDestillat(String destilatID,
+                                     String newMakeID,
+                                     double totalmaengeL,
+                                     double alkoholPc,
+                                     ArrayList<MaengdeDestilleret> maengder) {
+        Destillat destillat = new Destillat(destilatID, newMakeID, totalmaengeL, alkoholPc, maengder);
+        storage.addDestillat(destillat);
+        return destillat;
+    }
+
+    public ArrayList<Destillat> getDestillater() {
+        return storage.getDestillater();
+    }
+
+    public MaengdeDestilleret createMaengdeDestilleret(double liter,
+                                                       Destillering destillering,
+                                                       Destillat destillat) {
+        MaengdeDestilleret m = new MaengdeDestilleret(liter, destillering, destillat);
+        storage.addMaengdeDestilleret(m);
+        return m;
+    }
+
+    public ArrayList<MaengdeDestilleret> getMaengder() {
+        return storage.getMaengder();
+    }
+
+
+    public WhiskyProdukt createWhiskyProdukt(String produktNr,
+                                             String navn,
+                                             String beskrivelse,
+                                             double slutAlkoholProcent,
+                                             boolean erSingleCask,
+                                             int antalFlasker,
+                                             VandTilsaetning vandTilsaetning) {
+        WhiskyProdukt wp = new WhiskyProdukt(produktNr, navn, beskrivelse,
+                slutAlkoholProcent, erSingleCask,
+                antalFlasker, vandTilsaetning);
+        storage.addWhiskyProdukt(wp);
+        return wp;
+    }
+
+    public ArrayList<WhiskyProdukt> getWhiskyProdukter() {
+        return storage.getWhiskyProdukter();
+    }
+
+    public Aftapning createAftapning(String aftapningsNr,
+                                     double alkoholProcent,
+                                     LocalDate dato,
+                                     double volumenILiter,
+                                     Destillat destillat,
+                                     WhiskyProdukt whiskyProdukt) {
+        Aftapning a = new Aftapning(aftapningsNr, alkoholProcent, dato, volumenILiter, destillat, whiskyProdukt);
+        storage.addAftapning(a);
+        return a;
+    }
+
+    public ArrayList<Aftapning> getAftapninger() {
+        return storage.getAftapninger();
+    }
+
+    public VandTilsaetning createVandTilsaetning(String vandTilsaetning,
+                                                 double vandMaengde,
+                                                 String vandKilde,
+                                                 WhiskyProdukt whiskyProdukt) {
+        VandTilsaetning v = new VandTilsaetning(vandTilsaetning, vandMaengde, vandKilde, whiskyProdukt);
+        storage.addVandTilsaetning(v);
+        return v;
+    }
+
+    public ArrayList<VandTilsaetning> getVandTilsaetninger() {
+        return storage.getVandTilsaetninger();
+    }
+
+
+    public LagerMedarbejder createLagerMedarbejder(String medarbejderNr, String navn) {
+        LagerMedarbejder m = new LagerMedarbejder(medarbejderNr, navn);
+        storage.addLagerMedarbejder(m);
+        return m;
+    }
+
+    public ArrayList<LagerMedarbejder> getLagerMedarbejdere() {
+        return storage.getLagerMedarbejdere();
+    }
+
+    public Paafyldning createPaafyldning(String paafyldningstid,
+                                         double maengdeL,
+                                         double alkoholPcVedPaafyldning,
+                                         LocalDate dato,
+                                         LagerMedarbejder udfoertAf) {
+        Paafyldning p = new Paafyldning(paafyldningstid, maengdeL,
+                alkoholPcVedPaafyldning, dato, udfoertAf);
+        storage.addPaafyldning(p);
+        return p;
+    }
+
+    public ArrayList<Paafyldning> getPaafyldninger() {
+        return storage.getPaafyldninger();
+    }
 }
--- Projekt/src/Storage/Storage.java
+++ Projekt/src/Storage/Storage.java
@@ -5,13 +5,19 @@ import Model.*;
 import java.util.ArrayList;
 
 public class Storage {
+
     private ArrayList<Lager> lagre = new ArrayList<>();
+    private ArrayList<Reol> reoler = new ArrayList<>();
+    private ArrayList<Hylde> hylder = new ArrayList<>();
     private ArrayList<Fad> fade = new ArrayList<>();
-    private ArrayList<WhiskyProdukt> whiskyProdukter = new ArrayList<>();
     private ArrayList<Destillering> destilleringer = new ArrayList<>();
     private ArrayList<Destillat> destillater = new ArrayList<>();
+    private ArrayList<MaengdeDestilleret> maengder = new ArrayList<>();
+    private ArrayList<WhiskyProdukt> whiskyProdukter = new ArrayList<>();
+    private ArrayList<Aftapning> aftapninger = new ArrayList<>();
+    private ArrayList<VandTilsaetning> vandTilsaetninger = new ArrayList<>();
+    private ArrayList<Paafyldning> paafyldninger = new ArrayList<>();
     private ArrayList<LagerMedarbejder> lagerMedarbejdere = new ArrayList<>();
-
 
     public void addLager(Lager lager) {
         lagre.add(lager);
@@ -21,6 +27,21 @@ public class Storage {
         return lagre;
     }
 
+    public void addReol(Reol reol) {
+        reoler.add(reol);
+    }
+
+    public ArrayList<Reol> getReoler() {
+        return reoler;
+    }
+
+    public void addHylde(Hylde hylde) {
+        hylder.add(hylde);
+    }
+
+    public ArrayList<Hylde> getHylder() {
+        return hylder;
+    }
 
     public void addFad(Fad fad) {
         fade.add(fad);
@@ -30,16 +51,6 @@ public class Storage {
         return fade;
     }
 
-
-    public void addWhiskyProdukt(WhiskyProdukt produkt) {
-        whiskyProdukter.add(produkt);
-    }
-
-    public ArrayList<WhiskyProdukt> getWhiskyProdukter() {
-        return whiskyProdukter;
-    }
-
-
     public void addDestillering(Destillering d) {
         destilleringer.add(d);
     }
@@ -47,7 +58,6 @@ public class Storage {
     public ArrayList<Destillering> getDestilleringer() {
         return destilleringer;
     }
-
 
     public void addDestillat(Destillat d) {
         destillater.add(d);
@@ -57,6 +67,45 @@ public class Storage {
         return destillater;
     }
 
+    public void addMaengdeDestilleret(MaengdeDestilleret m) {
+        maengder.add(m);
+    }
+
+    public ArrayList<MaengdeDestilleret> getMaengder() {
+        return maengder;
+    }
+
+    public void addWhiskyProdukt(WhiskyProdukt w) {
+        whiskyProdukter.add(w);
+    }
+
+    public ArrayList<WhiskyProdukt> getWhiskyProdukter() {
+        return whiskyProdukter;
+    }
+
+    public void addAftapning(Aftapning a) {
+        aftapninger.add(a);
+    }
+
+    public ArrayList<Aftapning> getAftapninger() {
+        return aftapninger;
+    }
+
+    public void addVandTilsaetning(VandTilsaetning v) {
+        vandTilsaetninger.add(v);
+    }
+
+    public ArrayList<VandTilsaetning> getVandTilsaetninger() {
+        return vandTilsaetninger;
+    }
+
+    public void addPaafyldning(Paafyldning p) {
+        paafyldninger.add(p);
+    }
+
+    public ArrayList<Paafyldning> getPaafyldninger() {
+        return paafyldninger;
+    }
 
     public void addLagerMedarbejder(LagerMedarbejder m) {
         lagerMedarbejdere.add(m);
@@ -65,5 +114,4 @@ public class Storage {
     public ArrayList<LagerMedarbejder> getLagerMedarbejdere() {
         return lagerMedarbejdere;
     }
-
 }
